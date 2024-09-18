dc18510fa85489055d2b4e8cbd13fad56205d306
Vladimir <vova.drime@gmail.com>
2024-09-18 03:07:29 +0300

    feat: minimal structure

---
+++ src/pages/home/ui/home-page.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="text">Discover digital art & Collect NFTs</p>
+</template>
+
+<style lang="scss">
+.text {
+  font-weight: 600;
+  font-family: 'Work Sans', sans-serif;
+}
+</style>
---
+++ src/routes/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HomePage from '~/pages/home/ui/home-page.vue'
+</script>
+
+<template>
+  <HomePage/>
+</template>
