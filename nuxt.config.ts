cb530b5928279725886493fc20a1996adafba849
Vladimir <vova.drime@gmail.com>
2024-09-21 04:26:54 +0300

    feat: nuxt image config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,5 +43,15 @@ export default defineNuxtConfig({
 
   image: {
     dir: 'app/assets/images',
+    format: ['webp', 'png', 'jpg', 'jpeg'],
+    screens: {
+      'xs': 320,
+      'sm': 640,
+      'md': 768,
+      'lg': 1024,
+      'xl': 1280,
+      'xxl': 1536,
+      '2xl': 1536
+    },
   }
 })
